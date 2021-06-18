5239842c9ee208888da2565f4442ac3a519a4388
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-18 20:19:47 +0200

    creating basic struct of bookmarks component

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-bookmarks',
+  selector: 'Bookmarks',
   templateUrl: './bookmarks.component.html',
   styleUrls: ['./bookmarks.component.css']
 })
 export class BookmarksComponent implements OnInit {
-
+  title: string = 'Bookmarks +;
   constructor() { }
 
   ngOnInit(): void {
