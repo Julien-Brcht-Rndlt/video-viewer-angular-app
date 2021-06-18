397551af5028c3d26a89329c6d81ece3aaaabb67
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-18 20:19:07 +0200

    defining tag selector of the component history

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-history',
+  selector: 'History',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css'],
 })
