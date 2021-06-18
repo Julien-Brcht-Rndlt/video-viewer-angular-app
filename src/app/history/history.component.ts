a03aee05b5f72f562e5746c06c52f6c8e6e38c80
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-18 20:15:09 +0200

    creating basic struct of history component

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
-  styleUrls: ['./history.component.css']
+  styleUrls: ['./history.component.css'],
 })
 export class HistoryComponent implements OnInit {
+  title: string = 'History List';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
