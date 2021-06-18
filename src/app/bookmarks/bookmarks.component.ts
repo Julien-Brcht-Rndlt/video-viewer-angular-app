18fbe1d1a76e128943d488c51e64362092791507
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-18 20:35:47 +0200

    resolving missing single quote

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -3,13 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'Bookmarks',
   templateUrl: './bookmarks.component.html',
-  styleUrls: ['./bookmarks.component.css']
+  styleUrls: ['./bookmarks.component.css'],
 })
 export class BookmarksComponent implements OnInit {
-  title: string = 'Bookmarks +;
-  constructor() { }
+  title: string = 'Bookmarks +';
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
