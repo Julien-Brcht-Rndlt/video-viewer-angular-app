11639bc980a32e7eb125c9ee6ce5923ac51d3378
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-06-18 20:12:35 +0200

    creating basic struct of video-view component

--- src/app/video-view/video-view.component.ts
+++ src/app/video-view/video-view.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-video-view',
   templateUrl: './video-view.component.html',
-  styleUrls: ['./video-view.component.css']
+  styleUrls: ['./video-view.component.css'],
 })
 export class VideoViewComponent implements OnInit {
+  title: string = 'View Video';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
